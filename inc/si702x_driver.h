0268186bff973a68e1cffd4180030efb276d86d6
mikucukyilmaz <mi.kucukyilmaz@gmail.com>
2020-11-09 01:15:11 +0300

    Added func. prototypes.
    Function prototypes and register structs added.

--- inc/si702x_driver.h
+++ inc/si702x_driver.h
@@ -7,7 +7,13 @@
 
 #include "main.h"
 
-#define SI702x_I2C_ADDR (0x40 << 1)
+#define SI702x_I2C_ADDR     (0x40 << 1)
+#define SI702x_RH_TEMP_DIV  65536.0
+#define SI702x_RH_MULT      125.0
+#define SI702x_RH_OFFSET    6.0
+#define SI702x_TEMP_MULT    175.72
+#define SI702x_TEMP_OFFSET  46.85
+                                  
 /*
 #define SI702x_MEAS_RH_HOLD_MASTER      0xE5 // Measure Relative Humidity, Hold Master Mode
 #define SI702x_MEAS_RH_NO_HOLD_MASTER   0xF5 // Measure Relative Humidity, No Hold Master Mode
@@ -26,12 +32,72 @@
 #define SI702x_READ_FIRM_REV_1          0x84 // Read Firmware Revision Part 1
 #define SI702x_READ_FIRM_REV_2          0xB8 // Read Firmware Revision Part 2
 */
+
+typedef enum {
+  esample1  = 0x00,
+  si7013    = 0x00,
+  si7020    = 0x00,
+  si7021    = 0x00,
+  esample2  = 0xFF
+} si702x_dev_type_t;
+
+typedef enum {
+  firmware1_0  = 0xFF,
+  firmware2_0  = 0x20
+} si702x_fwrev_t;
+
 typedef struct {
-  _Bool isInit;
+  _Bool             isInit;
+  si702x_dev_type_t dev_type;
+  si702x_fwrev_t    fw_rev;
 } si702x_dev_t;
 
-int32_t si702x_init(void);
+// User Register 1 - Reset Settings = 0011_1010
+typedef union si702x_user_reg1_u {
+  uint8_t user_reg1;
+  struct {
+    uint8_t resolution0 :1;
+    uint8_t             :1;
+    uint8_t heater_en   :1;
+    uint8_t             :3;
+    uint8_t vdd_stat    :1;
+    uint8_t resolution1 :1;
+  } bits;
+} si702x_user_reg1_t;
 
+// Heater Control - Register Reset Settings = 0000_0000
+typedef union si702x_heater_ctrl_u {
+  uint8_t header_ctrl;
+  struct {
+    uint8_t resolution0 :4;
+    uint8_t             :3;
+  } bits;
+} si702x_heater_ctrl_t;
+
+
+
+// Device Config Registers
+int si702x_init(void);
+void si702x_reset(void);
+void si702x_set_user_reg1(void);
+void si702x_get_user_reg1(void);
+void si702x_set_heater_ctrl(void);
+void si702x_get_heater_ctrl(void);
+void si702x_read_id(void);
+void si702x_read_fw_rev(void);
+  
+// Device Data Registers
+void si702x_get_rh_no_hold(void);
+void si702x_get_rh_hold(void);
+
+void si702x_get_rh_no_hold(void);
+void si702x_get_rh_hold(void);
+
+void si702x_get_rh_temp_prev(void);
+
+static void si702x_calculate_rh(uint16_t rh_code);
+static void si702x_calculate_temp(uint16_t temp_code);
+  
 #ifdef __cplusplus
 }
 #endif
--- src/si702x_driver.c
+++ src/si702x_driver.c
@@ -1,7 +1,7 @@
-#include "Si702x_Driver.h"
+#include "si702x_Driver.h"
 
 // Device Method & Parameter Init
-int32_t si702x_init(void)
+int si702x_init(void)
 {
   int tmp = 0;
   return tmp;
